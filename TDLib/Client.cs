7e36d0c089d72d2a11d684868eca51d0cc5070b0
dantmnf <dantmnf@users.noreply.github.com>
2019-02-20 21:36:33 +0800

    cleanup

--- TDLib/Client.cs
+++ TDLib/Client.cs
@@ -50,24 +50,6 @@ namespace TDLib
             }
             return obj as T;
         }
-
-        /// <summary>
-        /// Execute function <paramref name="func"/> synchronously, cast the return value to <typeparamref name="T"/> or null.
-        /// <seealso cref="Execute(Function)"/>
-        /// </summary>
-        /// <typeparam name="T">The expected return type.</typeparam>
-        /// <param name="func">The function and parameters.</param>
-        /// <returns>The return value of <paramref name="func"/>.</returns>
-        /// <exception cref="TDLibError">Thrown if the function returns an <see cref="error"/></exception>
-        public T Execute<T>(Function func) where T : class
-        {
-            var obj = clientImpl.Execute((Function)func);
-            if (obj is Error e)
-            {
-                throw new TDLibError(e);
-            }
-            return obj as T;
-        }
         
         private long AddInvoke(TaskCompletionSource<TLObject> tsc)
         {
@@ -96,32 +78,13 @@ namespace TDLib
 
         /// <summary>
         /// <seealso cref="InvokeAsync(Function)"/>.
-        /// This overload returns the result as type <typeparamref name="T"/>,
-        /// or throws if the invocation returns an <see cref="error"/>.
-        /// </summary>
-        /// <typeparam name="T">The return type of <paramref name="func"/></typeparam>
-        /// <param name="func">The function and parameters to be invoked.</param>
-        /// <returns>A Task that will resolve to the return value of <paramref name="func"/> or null.</returns>
-        /// <exception cref="TDLibError">Thrown if the function returns an <see cref="error"/></exception>
-        public async Task<T> InvokeAsync<T>(Function func) where T : TLObject
-        {
-            var result = await InvokeAsync((Function)func);
-            if (result is Error e)
-            {
-                throw new TDLibError(e);
-            }
-            return result as T;
-        }
-
-        /// <summary>
-        /// <seealso cref="InvokeAsync(Function)"/>.
         /// This overload returns the type defined by <paramref name="func"/>,
-        /// or throws if the invocation returns an <see cref="error"/>.
+        /// or throws if the invocation returns an <see cref="Error"/>.
         /// </summary>
         /// <typeparam name="T">The return type of <paramref name="func"/></typeparam>
         /// <param name="func">The function and parameters to be invoked.</param>
         /// <returns>A Task that will resolve to the return value of <paramref name="func"/>.</returns>
-        /// <exception cref="TDLibError">Thrown if the function returns an <see cref="error"/></exception>
+        /// <exception cref="TDLibError">Thrown if the function returns an <see cref="Error"/></exception>
         public async Task<T> InvokeAsync<T>(Function<T> func) where T : TLObject
         {
             var result = await InvokeAsync((Function)func);
@@ -136,15 +99,15 @@ namespace TDLib
         public Task<Update> WaitForUpdate(Func<Update, bool> criterion, CancellationToken ct = default(CancellationToken))
         {
             var tsc = new TaskCompletionSource<Update>();
-            EventHandler<Update> handler = null;
-            handler = (sender, u) =>
+            void handler(object sender, Update u)
             {
                 if (criterion(u))
                 {
                     Update -= handler;
                     tsc.TrySetResult(u);
                 }
-            };
+            }
+
             Update += handler;
             ct.Register(() =>
             {
@@ -178,7 +141,7 @@ namespace TDLib
         }
 
         /// <summary>
-        /// Starts the tdlib event loop. Required for <see cref="InvokeAsync(Function)"/>, <see cref="InvokeAsync{T}(Function)"/>, <see cref="InvokeAsync{T}(Function{T})"/>.
+        /// Starts the tdlib event loop. Required for <see cref="InvokeAsync(Function)"/>, <see cref="InvokeAsync{T}(Function{T})"/>.
         /// </summary>
         /// <returns>The event loop Task.</returns>
         public Task Run()
