7132bc5cebddfb805ec1c68e95b6c0b0748c719c
dantmnf <dantmnf@users.noreply.github.com>
2020-02-03 16:17:56 +0800

    add native crc32

--- TDLib.JsonClient/Crc32.cs
+++ TDLib.JsonClient/Crc32.cs
@@ -2,30 +2,70 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Runtime.InteropServices;
+using System.Security;
 
 namespace TDLib.JsonClient
 {
-
-    internal static class Crc32
+    [SuppressUnmanagedCodeSecurity]
+    internal static unsafe class Crc32
     {
+        private delegate uint zlib_crc32_t(uint crc, byte* buf, int len);
+
+        [DllImport("zlib1.dll", EntryPoint = "crc32", CallingConvention = CallingConvention.Cdecl)]
+        private static extern uint zlib_crc32_windows(uint crc, byte* buf, int len);
+        [DllImport("zlib.dll" /* buggy CMake product */, EntryPoint = "crc32", CallingConvention = CallingConvention.Cdecl)]
+        private static extern uint zlib_crc32_windows2(uint crc, byte* buf, int len);
+        [DllImport("libz.so.1" /* LSB */, EntryPoint = "crc32", CallingConvention = CallingConvention.Cdecl)]
+        private static extern uint zlib_crc32_linux(uint crc, byte* buf, int len);
+        [DllImport("libz.1.dylib" /* built-in */, EntryPoint = "crc32", CallingConvention = CallingConvention.Cdecl)]
+        private static extern uint zlib_crc32_darwin(uint crc, byte* buf, int len);
+
         private static readonly uint[] table;
         private const uint poly = 0xedb88320u;
+
+        private static zlib_crc32_t zlib_crc32;
+
+
         static Crc32()
         {
-            table = new uint[16 * 256];
-            for (uint i = 0; i < 256; i++)
+            zlib_crc32 = CheckDllCall(zlib_crc32_windows, zlib_crc32_windows2, zlib_crc32_linux, zlib_crc32_darwin);
+            if (zlib_crc32 == null)
             {
-                uint res = i;
-                for (int t = 0; t < 16; t++)
+                table = new uint[16 * 256];
+                for (uint i = 0; i < 256; i++)
                 {
-                    for (int k = 0; k < 8; k++) res = (res & 1) == 1 ? poly ^ (res >> 1) : (res >> 1);
-                    table[(t * 256) + i] = res;
+                    uint res = i;
+                    for (int t = 0; t < 16; t++)
+                    {
+                        for (int k = 0; k < 8; k++) res = (res & 1) == 1 ? poly ^ (res >> 1) : (res >> 1);
+                        table[(t * 256) + i] = res;
+                    }
                 }
             }
         }
 
+        private static zlib_crc32_t CheckDllCall(params zlib_crc32_t[] delgs)
+        {
+            foreach (var delg in delgs)
+            {
+                try
+                {
+                    delg.Invoke(0, null, 0);
+                    return delg;
+                }
+                catch (DllNotFoundException e) { }
+            }
+            return null;
+        }
+
         public static uint Update(uint crc, ReadOnlySpan<byte> input)
         {
+            if (zlib_crc32 != null)
+            {
+                fixed (byte* buf = input)
+                    return zlib_crc32.Invoke(crc, buf, input.Length);
+            }
             uint crcLocal = uint.MaxValue ^ crc;
             var length = input.Length;
             var offset = 0;
@@ -62,16 +102,9 @@ namespace TDLib.JsonClient
 
             return crcLocal ^ uint.MaxValue;
         }
-
-        public static uint Update(uint crc, byte input)
-        {
-            uint crcLocal = uint.MaxValue ^ crc;
-            crcLocal = table[(byte)(crcLocal ^ input)] ^ crcLocal >> 8;
-            return crcLocal ^ uint.MaxValue;
-        }
     }
 
-    internal class Crc32Stream : Stream
+    internal unsafe class Crc32Stream : Stream
     {
         public uint Hash;
 
@@ -87,7 +120,7 @@ namespace TDLib.JsonClient
 
         public override void WriteByte(byte value)
         {
-            Hash = Crc32.Update(Hash, value);
+            Hash = Crc32.Update(Hash, new ReadOnlySpan<byte>(&value, 1));
         }
         public override void Write(byte[] buf, int off, int len)
         {
